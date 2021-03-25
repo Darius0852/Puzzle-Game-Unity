67a43c2c7de0579d61295f0d3abb519e003a450e
Darius Ghomashchian <Darius0852@yahoo.co.uk>
2021-03-25 23:54:47 +0000

    initial

---
+++ PuzzleGame/Assets/Scripts/BlockMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockMovement : MonoBehaviour
+{
+    private float speedUp;
+    public float Speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speedUp = 1.0F;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        playerMovement();
+
+    }
+
+    void playerMovement() {
+
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 playerMovement = new Vector3(ver, 0f, -hor) * Speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+
+    }
+
+    public void SpeedUp()
+    {
+        speedUp = 3.0F;
+    }
+
+    public void SlowDown()
+    {
+        speedUp = 1.0F;
+    }
+}
---
+++ PuzzleGame/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerCamera : MonoBehaviour
+{
+
+    public float RotationSpeed = 4;
+    public Transform centrePoint, Player;
+    float mouseX, mouseY;
+
+    public Transform Obstruction;
+    float zoomSpeed = 2f;
+
+
+    public GameObject objectToFollow;
+    public float xrotation = 1.0F;
+    public float speed = 2.0F;
+    public float ydist = 4.0F;
+    private Vector3 rotateValue;
+    private Vector3 moveValue;
+    private float offset;
+    public bool isInverted = false;
+
+    void Start()
+    {
+        Obstruction = centrePoint;
+        isInverted = objectToFollow.GetComponent<GravityBlock>().getIsInverted();
+        
+    }
+
+    void LateUpdate()
+    {
+
+        CamControl();
+        ViewObstructed();
+        isInverted = objectToFollow.GetComponent<GravityBlock>().getIsInverted();
+        float interpolation = speed * Time.deltaTime;
+
+
+        Debug.Log(isInverted);
+
+        if (isInverted == true)
+        {
+            rotateValue = new Vector3(-30, -90, 0);
+            transform.eulerAngles = rotateValue;
+            ydist = -4.0F;
+            offset = transform.position.y;
+            moveValue = new Vector3(transform.position.x, offset, transform.position.z);
+            transform.position = moveValue;
+        }
+        else if (isInverted == false)
+        {
+            
+        }
+
+    }
+
+    void CamControl()
+    {
+
+        mouseX += Input.GetAxis("Mouse X") * RotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * RotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        transform.LookAt(centrePoint);
+
+        centrePoint.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        Player.rotation = Quaternion.Euler(0, mouseX, 0);
+
+    }
+
+    void ViewObstructed()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, centrePoint.position - transform.position, out hit, 4.5f))
+        {
+            if (hit.collider.gameObject.tag != "Player")
+            {
+                Obstruction = hit.transform;
+                Obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+
+                if(Vector3.Distance(Obstruction.position, transform.position) >= 3f && Vector3.Distance(transform.position, centrePoint.position) >= 1.5f)
+                {
+                    transform.Translate(Vector3.forward * zoomSpeed * Time.deltaTime);
+                }
+            }
+            else {
+
+                Obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+                if(Vector3.Distance(transform.position, centrePoint.position) < 4.5f)
+                {
+                    transform.Translate(Vector3.back * zoomSpeed * Time.deltaTime);
+                }
+
+            }
+
+
+        }
+
+
+    }
+
+}
